f91856874d937543a699ffbbab196dd27e479686
Edward Bridges <github@eqbridges.com>
2019-10-29 20:58:41 -0400

    configure cli arg parser

--- lgw/main.py
+++ lgw/main.py
@@ -1,5 +1,21 @@
-def main():
+'''
+Lambda Gateway.
+
+Usage:
+  lgw deploy-api --config-file=<cfg>
+
+Options:
+  -h --help             Show this screen.
+  --version             Show version.
+  --config-file=<cfg>   Override defaults with these settings.
+'''
+
+from docopt import docopt
+
+
+def main(args):
   print('Hello World!')
   
 if __name__== '__main__':
-  main()
+  args = docopt(__doc__, version=__version__)
+  main(args)
