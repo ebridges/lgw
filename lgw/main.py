c14a8ca153e39fb896c7a3ce4176f7757f1fcc9f
Edward Bridges <github@eqbridges.com>
2019-10-29 21:04:04 -0400

    configure logging

--- lgw/main.py
+++ lgw/main.py
@@ -10,12 +10,17 @@ Options:
   --config-file=<cfg>   Override defaults with these settings.
 '''
 
+from logging import info, debug, error
 from docopt import docopt
+from .util import configure_logging
+from . import __version__
 
+def app():
+  info('Hello World!')
 
-def main(args):
-  print('Hello World!')
-  
-if __name__== '__main__':
+def main():
   args = docopt(__doc__, version=__version__)
-  main(args)
+  configure_logging(args.get('--verbose'))
+
+if __name__== '__main__':
+  main()
