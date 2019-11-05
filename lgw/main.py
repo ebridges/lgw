222e0a0caee287ddffbc962216cc50102d93e97f
Edward Bridges <github@eqbridges.com>
2019-11-05 07:17:40 -0500

    add arguments & config to app to support lambda lifecycle mgmt

--- lgw/main.py
+++ lgw/main.py
@@ -3,15 +3,22 @@ Lambda Gateway.
 
 Usage:
   lgw deploy-api [--verbose] --config-file=<cfg>
+  lgw lambda-deploy [--verbose] --config-file=<cfg> --lambda-file=<zip>
+  lgw lambda-invoke [--verbose] --lambda-name=<name> [--payload=<json>]
+  lgw lambda-delete [--verbose] --lambda-name=<name>
 
 Options:
   -h --help             Show this screen.
   --version             Show version.
   --verbose             Enable DEBUG-level logging.
   --config-file=<cfg>   Override defaults with these settings.
+  --lambda-file=<zip>   Path to zip file with executable lambda code.
+  --lambda-name=<name>  Name of the lambda to invoke or delete.
+  --payload=<json>      Path to a file of type json with data to send with the lambda invocation.
 '''
 
 from os import path
+import json
 from logging import info, debug, error
 from everett.manager import ConfigManager, ConfigOSEnv, ConfigEnvFileEnv, ConfigDictEnv
 from docopt import docopt
@@ -19,23 +26,92 @@ from lgw.util import configure_logging
 from lgw.version import __version__
 from lgw import settings
 from lgw.api_gateway import create_rest_api
+from lgw.lambda_util import deploy_function, invoke_function, delete_function
+
+
+def handle_deploy_lambda(file, config):
+    info('handle_deploy_lambda() called with file [%s]' % file)
+    if not path.isfile(file):
+        error('ERROR: Lambda zip file not found at location: [%s]' % file)
+        return
+    if not file.endswith('.zip'):
+        error('ERROR: Lambda file expected to be in ZIP format.')
+        return
+
+    lambda_arn = deploy_function(
+        file,
+        config('aws_lambda_name'),
+        config('aws_lambda_handler'),
+        config('aws_lambda_execution_role_arn'),
+        config('aws_lambda_connection_timeout'),
+        config('aws_lambda_memory_size'),
+        config('aws_lambda_runtime'),
+        config('aws_lambda_archive_bucket'),
+        config('aws_lambda_archive_key'),
+        config('aws_lambda_description'),
+        config('aws_lambda_vpc_subnets'),
+        config('aws_lambda_vpc_security_groups'),
+        config('aws_lambda_environment'),
+        config('aws_lambda_tags'),
+    )
+    print(lambda_arn)
+    info('Lambda [%s] created.' % config('aws_lambda_name'))
+    return 1
+
+
+def handle_invoke_lambda(name, payload):
+    info('handle_invoke_lambda() called for lambda [%s]' % name)
+    if payload:
+        result = invoke_function(name, payload)
+    else:
+        result = invoke_function(name)
+    json_result = json.loads(result['Payload'].read().decode('utf-8'))
+    print(json_result)
+    info('Invocation completed for lambda [%s]' % name)
+    return 1
+
+
+def handle_delete_lambda(name):
+    info('handle_delete_lambda() called for lambda [%s]' % name)
+    delete_function(name)
+    info('Lambda [%s] deleted.' % name)
+    return 1
+
+
+def handle_deploy_api_gateway(config):
+    api_url = create_rest_api(
+        config('aws_api_name'),
+        config('aws_lambda_name'),
+        config('aws_api_resource_path'),
+        config('aws_api_deploy_stage'),
+    )
+    print(api_url)
+    info('REST API URL: [%s]' % api_url)
+    return 1
 
 
 def app(args, config):
     if args.get('deploy-api'):
-        api_url = create_rest_api(
-            config('aws_api_name'),
-            config('aws_lambda_name'),
-            config('aws_api_resource_path'),
-            config('aws_api_deploy_stage'),
-        )
-        info('REST API URL: [%s]' % api_url)
+        return handle_deploy_api_gateway(config)
+    if args.get('lambda-deploy'):
+        file = path.abspath(args.get('--lambda-file'))
+        return handle_deploy_lambda(file, config)
+    if args.get('lambda-invoke'):
+        name = args.get('--lambda-name')
+        payload = args.get('--payload', None)
+        return handle_invoke_lambda(name, payload)
+    if args.get('lambda-delete'):
+        name = args.get('--lambda-name')
+        return handle_delete_lambda(name)
 
+    error('Unrecognized command.')
 
 def main():
     args = docopt(__doc__, version=__version__)
     configure_logging(args.get('--verbose'))
-    config_file = args.get('--config-file')
+    config_file = args.get('--config-file', None)
+    if config_file:
+        config_file = path.abspath(config_file)
 
     config = ConfigManager(
         [
--- lgw/settings.py
+++ lgw/settings.py
@@ -1,8 +1,20 @@
 def defaults():
   return {
     'aws_region': 'eu-west-1',
-    'aws_lambda_name': '',
     'aws_api_name': '',
     'aws_api_resource_path': '',
     'aws_api_deploy_stage': '',
+    'aws_lambda_name': '',
+    'aws_lambda_description': '',
+    'aws_lambda_handler': '',
+    'aws_lambda_runtime': 'python3.7',
+    'aws_lambda_connection_timeout': 30,
+    'aws_lambda_memory_size': 3000,
+    'aws_lambda_archive_bucket': '',
+    'aws_lambda_archive_key': '',
+    'aws_lambda_execution_role_arn': '',
+    'aws_lambda_vpc_subnets': '',
+    'aws_lambda_vpc_security_groups': '',
+    'aws_lambda_environment': '',
+    'aws_lambda_tags': '',
   }
