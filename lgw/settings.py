88b1eb492317667b941a0ce04bdf7f759ec1e9a5
Edward Bridges <github@eqbridges.com>
2019-11-10 15:06:51 -0500

    change to int param

--- lgw/settings.py
+++ lgw/settings.py
@@ -6,7 +6,7 @@ def defaults():
     'aws_api_deploy_stage': '',
     'aws_api_domain_name': '',
     'aws_api_base_path': '(none)',
-    'aws_api_domain_wait_until_available': True,
+    'aws_api_domain_wait_until_available': 40, # minutes
     'aws_acm_certificate_arn': '',
     'aws_lambda_name': '',
     'aws_lambda_description': '',
